a79f64d03e317e5135c66a1cfc251ef601231b58
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-10 18:45:11 -0300

    Class Base to dict implementation

--- models/base_model.py
+++ models/base_model.py
@@ -11,16 +11,16 @@ class BaseModel:
     def __init__(self):
         """Constructor"""
         self.id = str(uuid4())
-        self.created_at = datatime.now()
-        self.updated_at = datatime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """String expression of object"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates ins att with current datatime"""
-        self.updated_at = datatime.now()
+        """Updates ins att with current datetime"""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Dictionary with keys/values of __dict__ of the instance"""
