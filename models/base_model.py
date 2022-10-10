61b08dece72a39e3b2bf05c11b3a6ca26ed7769d
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-10 20:36:17 -0300

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
         """Constructor"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
+                if key in ("created_at", "updated_at"):
                     self.__dict__[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 else:
                     self.__dict__[key] = value
