94f844d06423d97bccf1fd67cdd4f3b539e5407a
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-11 14:52:45 -0300

    Update console

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module Console
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class for command interpreter"""
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Take empty line""" 
+        pass
+
+    def do_quit(self, args):
+        """Exit program"""
+        return True
+
+    def do_EOF(self, args):
+        """End of file"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+    console.cmdloop()
