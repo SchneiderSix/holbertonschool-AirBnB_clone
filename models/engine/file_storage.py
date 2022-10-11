eaf93e2fc62eb4246c49d929e1b5d03f9fbf0ba1
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-10 22:21:21 -0300

    File storage functions

---
+++ models/engine/file_storage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module File Storage
+"""
+import json
+
+
+class FileStorage:
+    """Class FileStorage"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns dic of obj"""
+        return self.__objects
+
+    def new(self, obj):
+        """Set new object"""
+        self.__objects[obj.__class__.__name__ + '.' + str(obj)] = obj
+
+    def save(self):
+        """Serializes to json"""
+        with open(self.__file_path, 'w') as sf:
+            json.dump(sf)
+
+    def reload(self):
+        """Deserializes from json"""
+        if self.__file_path:
+            with open(self.__file_path) as rf:
+                json.load(rf)
