6b30af432eeb4084fc467e18f50b3d7137c0ea2e
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-11 00:13:57 -0300

    File storage reload method fix with new method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,22 +18,11 @@ class FileStorage:
         """Set new object"""
         self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
 
-    def o_dict(self):
-        """Dictionary with keys/values of __dict__ of the instance"""
-        el = {}
-        for key, value in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                el[key] = value.isoformat()
-            else:
-                el[key] = value
-            el["__class__"] = self.__class__.__name__
-        return el
-
     def save(self):
         """Serializes to json"""
         ob = {}
         for key in self.__objects:
-            ob[key] = self.__objects[key].o_dict()
+            ob[key] = self.__objects[key].dict()
         with open(self.__file_path, 'w') as sf:
             json.dump(ob, sf)
 
