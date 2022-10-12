04ec16d54caef79e986fd4facb53b2065553575e
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-12 12:28:46 -0300

    Console implementation create, show, destroy

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 Module Console
 """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +21,43 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, args):
         """End of file"""
         return True
+
+    def do_create(self, args):
+        """Create instance of BaseModel"""
+        if args == "BaseModel":
+            pass
+        elif args == '':
+            "** class name missing **"
+        else:
+            "** class doesn't exist **"
+
+    def do_show(self, args):
+        """Show string representation of object"""
+        if isinstance(args[0], BaseModel) and args[1] == args[0].id:
+            f"{args[0].__str__()}"
+        elif args[0] == '':
+            "** class name missing **"
+        elif args[1] == '':
+            "** instance id missing **"
+        elif isinstance(args[0], BaseModel) is False:
+            "** class doesn't exist **"
+        else:
+            "** no instance found **"
+
+    def do_destroy(self, args):
+        """Destroy object"""
+        if isinstance(args[0], BaseModel) and args[1] == args[0].id:
+            pass
+        elif args[0] == '':
+            "** class name missing **"
+        elif args[1] == '':
+            "** instance id missing **"
+        elif isinstance(args[0], BaseModel) is False:
+            "** class doesn't exist **"
+        else:
+            "** no instance found **"
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
