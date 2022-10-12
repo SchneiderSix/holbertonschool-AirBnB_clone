0c6b5ef9b6c677745a57e96a7f65f5dd68e629e5
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-12 16:00:47 -0300

    Add dict calsses to file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,20 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    def classx(self):
+        """Dict with all calsses"""
+        from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
+
+        classes = {"BaseModel": BaseModel, "Amenity": Amenity,
+                "City": City, "Place": Place,
+                "Review": Review, "State": State, "User": User}
+        return classes
     def all(self):
         """Returns dic of obj"""
         return self.__objects
