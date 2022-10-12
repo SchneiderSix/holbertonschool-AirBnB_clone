769ba236858886f182f6651da986b9310f565047
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-12 14:47:45 -0300

    Implement amenity, city, place, review, state and user classes

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module Review
+"""
+from base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Class Review"""
+    place_id = ''
+    user_id = ''
+    text = ''
