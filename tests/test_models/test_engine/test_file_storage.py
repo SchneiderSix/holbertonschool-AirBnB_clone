ed30e0a7aec08c2c62a98cb54307ea4225a80fd7
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-13 17:21:08 -0300

    Update tests imports

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,7 +3,7 @@
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-from .. import storage
+from . import storage
 
 
 class TestFileStorage(unittest.TestCase):
