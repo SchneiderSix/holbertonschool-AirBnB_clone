9863b8647e2c3d60c3e6ae3477ebb49ad6184753
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-13 15:17:30 -0300

    Tests for file_storage

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module Test file_storage"""
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Testing FileStorage"""
+    def test_filestorage(self):
+        my_model = FileStorage()
+        my_model_list = storage.all()
+        self.assertEqual(type(my_model_list), list)
