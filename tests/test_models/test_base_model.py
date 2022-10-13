1240455160f94b6e654e7b3c8a54017e3f7d2fbd
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-13 17:19:34 -0300

    Update tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,8 +5,7 @@
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-from models.engine.file_storage import storage
-
+import storage
 
 class TestBaseModel(unittest.TestCase):
     """Testing base_model"""
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,7 +3,7 @@
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-from models.engine.file_storage import storage
+import storage
 
 
 class TestFileStorage(unittest.TestCase):
