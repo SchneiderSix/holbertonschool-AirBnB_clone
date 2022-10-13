39eb2cc1213ecd9eb448bde8e3b96ac5653eb7ac
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-13 16:45:23 -0300

    Update test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,13 +15,12 @@ class TestBaseModel(unittest.TestCase):
         my_model.my_number = 89
         my_model.save()
         my_model_dict = my_model.to_dict()
-        self.assertEqual(type(my_model_dict), dict)
         self.assertEqual(my_model.__class__.__name__, "BaseModel")
         self.assertEqual(my_model.name, "My First Model")
         self.assertEqual(my_model.my_number, 89)
         self.assertTrue(isinstance(my_model.created_at, datetime))
         self.assertTrue(isinstance(my_model.updated_at, datetime))
-
+        self.assertEqual(type(my_model.__dict__), dict)
 
 if __name__ == "__main__":
     unittest.main()
