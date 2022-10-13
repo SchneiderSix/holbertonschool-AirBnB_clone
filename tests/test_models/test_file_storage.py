f167505b60a9048708698b812ada7a17e1f654bc
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-13 15:22:48 -0300

    Update Tests for file_storage

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module Test file_storage"""
 import unittest
-from models.engine.file_storage import FileStorage
+import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
