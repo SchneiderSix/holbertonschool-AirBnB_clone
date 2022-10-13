28a9e6b778109e57bef6b2b3debd3549b57cb301
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-13 16:39:26 -0300

    Update test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,17 +10,17 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
     """Testing base_model"""
     def test_BaseModel_save(self):
-        my_model = BaseModel()
-        my_model.name = "My First Model"
-        my_model.my_number = 89
-        my_model.save()
-        my_model_dict = my_model.to_dict()
-        self.assertEqual(my_model.__class__.__name__, "BaseModel")
-        self.assertEqual(my_model.name, "My First Model")
-        self.assertEqual(my_model.my_number, 89)
-        self.assertTrue(isinstance(my_model.created_at, datetime))
-        self.assertTrue(isinstance(my_model.updated_at, datetime))
-        self.assertEqual(type(my_model_dict), dict)
+        bm = BaseModel()
+        bm.name = "My First Model"
+        bm.my_number = 89
+        bm.save()
+        bm_dict = bm.to_dict()
+        self.assertEqual(bm.__class__.__name__, "BaseModel")
+        self.assertEqual(bm.name, "My First Model")
+        self.assertEqual(bm.my_number, 89)
+        self.assertTrue(isinstance(bm.created_at, datetime))
+        self.assertTrue(isinstance(bm.updated_at, datetime))
+        self.assertEqual(type(bm_dict), dict)
 
 if __name__ == "__main__":
     unittest.main()
