bd3bb6a8308c7281856bf63b1f05ad86f0dcc112
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-13 16:17:20 -0300

    Fixing tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,7 +15,7 @@ class TestBaseModel(unittest.TestCase):
         my_model.save()
         my_model_dict = my_model.to_dict()
         self.assertEqual(my_model.__class__.__name__, "BaseModel")
-        self.assertEqual(my_model.name, "My Fisrt Model")
+        self.assertEqual(my_model.name, "My First Model")
         self.assertEqual(my_model.my_number, 89)
         self.assertTrue(isinstance(my_model.created_at, datetime))
         self.assertTrue(isinstance(my_model.updated_at, datetime))
